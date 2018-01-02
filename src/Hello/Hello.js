dca67040d25bb90c8ed4b7a32c5447b93266080a
Natalia Garrido <natgramirez@outlook.com>
2018-01-01 23:48:17 -0300

    inicializando proyecto curso de udemy react+redux

---
+++ src/Hello/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>Hello desde un componente de React</div>
+    )
+  }
+}
+
+export default Hello
